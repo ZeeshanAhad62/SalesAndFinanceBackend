6f96e82e1ca92f46dc02fb33bcd249b0e7a07142
ZeeshanAhad62 <ZeeshanAhad62@gmail.com>
2025-03-01 05:30:30 +0500

    add db context class

--- SalesAndFinance.Infrastructure/obj/Debug/net8.0/SalesAndFinance.Infrastructure.AssemblyInfo.cs
+++ SalesAndFinance.Infrastructure/obj/Debug/net8.0/SalesAndFinance.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesAndFinance.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34033f1715367c5d194be6141592a56ccd1e681")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesAndFinance.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesAndFinance.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SalesAndFinance/Program.cs
+++ SalesAndFinance/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using SalesAndFinance.Infrastructure.Data;
+
 namespace SalesAndFinance
 {
     public class Program
@@ -8,6 +11,8 @@ namespace SalesAndFinance
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<SalesAndFinanceDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
